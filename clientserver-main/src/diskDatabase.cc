ba1ea2bd41553b9a7f7842c79ecaa83ef887ac7e
Max <ma8251so-s@student.lu.se>
2022-05-04 14:00:51 +0200

    changed initdb

--- clientserver-main/src/diskDatabase.cc
+++ clientserver-main/src/diskDatabase.cc
@@ -6,6 +6,7 @@
 #include <map>
 #include <filesystem>
 #include <iostream>
+#include <fstream>
 //#include <sys/stat.h>
 
 
@@ -23,19 +24,27 @@ DiskDatabase::DiskDatabase(const string& path) : path(path)
 void DiskDatabase::initDb() {
     if (!fs::exists(path)) {
         fs::create_directory(path);
+        
+        string dir = path;
+        dir.append("idnewsgroupsmap.txt");
+        std::cout << dir << std::endl;
+        std::fflush(stdout);
+        std::ofstream o(dir);
+        o << "Hello, World!" << std::endl;
+        o.close();
     }
+
 }
 
 
 void DiskDatabase::createNewsgroup(string title)
 {
-    
+
 }
 
 map<int, Newsgroup> DiskDatabase::getNewsgroups()
 {
-    // Get all subdirs
-    // return newsgroups;
+
 }
 
 string DiskDatabase::deleteNewsgroup(int newsgroupId)
